81d527a8072a4495218e8d544b64341b02b97fbe
ifaridaiman <ifaridaiman@gmail.com>
2025-03-08 06:15:05 +0800

    chore: update the styling for mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-url('/assets/background/lemang-bg.png') bg-cover bg-center bg-no-repeat h-screen grid grid-rows-[auto,1fr,auto]">
       <main className="flex flex-col gap-8 row-start-2 justify-center items-center ">
         <Image
           className="dark:invert"
@@ -12,12 +12,14 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1 className="text-9xl font-bold text-center text-white">
-          COMING SOON
-        </h1>
-        <p className="text-4xl font-light text-center text-white">
-          100% buatan Saujana Utama
-        </p>
+        <div>
+          <h1 className="text-5xl md:text-9xl font-bold text-center text-white">
+            COMING SOON
+          </h1>
+          <p className="text-base md:text-4xl font-light text-center text-white mt-4">
+            100% buatan Saujana Utama
+          </p>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center text-center">
         <p className="text-xs text-gray-50">Develop by Naisu Technologies</p>
